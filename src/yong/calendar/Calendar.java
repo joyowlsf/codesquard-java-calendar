b42f735fb5c248bf5cae178427a6d127e1c00c68
winsult <winsult@DESKTOP-HOR35HN>
2018-05-21 00:24:58 +0900

    My Calendar........

--- src/yong/calendar/Calendar.java
+++ src/yong/calendar/Calendar.java
@@ -1,20 +1,40 @@
 package yong.calendar;
 import java.util.Scanner;
 public class Calendar {
-
-	public static void main(String[] args) {
+	
+	public static void Monthprint28() {
 		System.out.println("일 월 화 수 목 금 토 일");
 		System.out.println("--------------------");
 		System.out.println(" 1  2  3  4  5  6  7");
 		System.out.println(" 8  9 10 11 12 13 14");
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
-		
-		//숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
-		Scanner scanner = new Scanner(System.in);
+	}
 	
-		int maxDays[]= {31,28,31,30,31,30,31,31,30,31,30,31};
-		//반복 입력 가능하도록 프로그램 변경하기
+	public static void Monthprint30()
+	{
+		System.out.println("일 월 화 수 목 금 토 일");
+		System.out.println("--------------------");
+		System.out.println(" 1  2  3  4  5  6  7");
+		System.out.println(" 8  9 10 11 12 13 14");
+		System.out.println("15 16 17 18 19 20 21");
+		System.out.println("22 23 24 25 26 27 28");
+		System.out.println("29 30 ");
+	}
+	
+	public static void Monthprint31()
+	{
+		System.out.println("일 월 화 수 목 금 토 일");
+		System.out.println("--------------------");
+		System.out.println(" 1  2  3  4  5  6  7");
+		System.out.println(" 8  9 10 11 12 13 14");
+		System.out.println("15 16 17 18 19 20 21");
+		System.out.println("22 23 24 25 26 27 28");
+		System.out.println("29 30 31 ");
+	}
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 	
 		while(true)
 		{
@@ -26,7 +46,22 @@ public class Calendar {
 				System.out.println("Have a nice day!");
 				break;
 			}
-			System.out.println(month+"월은"+maxDays[month-1]+"있습니다.");
+			else if(month==2)
+			{
+				Monthprint28();
+			}
+			else if(month==4||month==6||month==9||month==11)
+			{
+				Monthprint30();
+			}
+			else if(month==1||month==3||month==5||month==7||month==8||month==10||month==12)
+			{
+				Monthprint31();
+			}
+			else if(month==-1)
+			{
+				break;
+			}
 		}
 		scanner.close();
 	}
---
+++ src/yong/calendar/Prompt.java
@@ -0,0 +1,10 @@
+package yong.calendar;
+
+public class Prompt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
